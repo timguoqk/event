a55f6691d552b5d2f84ccdf548e5cf2ccd0d920a
Linda Chang <bairnowl@Lindas-MacBook-Air.local>
2016-02-06 19:11:31 -0500

    MLhelper

--- MLhelper.js
+++ MLhelper.js
@@ -1,17 +1,20 @@
-var sys = require('sys')
-var exec = require('child_process').exec;
+var sys = require('util')
+var child_process = require('child_process');
 
 // http://stackoverflow.com/questions/2496710/writing-files-in-node-js
 
 module.exports = {
 //https://dzone.com/articles/execute-unix-command-nodejs
-  create: function() {
-    
+  create: function(userID, initialData) {
+	var buffer = child_process.execSync('python TODO');
+	return buffer.toString();
   },
-  train: function() {
-
+  train: function(userID, trainData) {
+	var buffer = child_process.execSync('python TODO');
+	return buffer.toString();
   },
-  predict: function() {
-
+  predict: function(userID, data) {
+	var buffer = child_process.execSync('python TODO');
+	return buffer.toString();
   }
 };
---
+++ temp.js
@@ -0,0 +1,4 @@
+var MLhelper = require('./MLhelper');
+
+var temp = MLhelper.create('python ml.py');
+console.log(temp);
